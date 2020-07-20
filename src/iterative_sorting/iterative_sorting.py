32571abe8dcc9b3934d367a99d668bd41e8fc1d0
toricruzmendiola <mtoricruz@gmail.com>
2020-07-20 11:00:07 -0700

    pseudo code for selection sort complete

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -4,21 +4,32 @@ def selection_sort(arr):
     # len(arr) - 1 bc once there's 1 item left in 
     # unsorted list, we don't have to run a comparison on it.
     #  Assume it's the highest value left
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
-        for elem in range(i+1, len(arr)):
-            if arr[elem] < arr[smallest_index]:
-                smallest_index = elem
 
-        if smallest_index != i:
-            arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+    # for every element in the length of the array
+    for i in range(0, len(arr) - 1):
+        # preserve current index variable for i
+        cur_index = i
+        # preserve smallest index variable in cur_index
+        smallest_index = cur_index
+        # each iteration we want the first element in the unsorted list
+        # to be the default min
+        
+        # TO-DO: find next smallest element
+        # for every element in the range i+1 (means to the right of the index) to the 
+        # length of the array
+        for elem in range(i+1, len(arr)):
+            # if the elem positions value of the array is smaller than the current value of the smallest_index variable
+            if arr[elem] < arr[smallest_index]:
+                # replace the smallest index value with the current elem value
+                smallest_index = elem
 
         # TO-DO: swap
         # Your code here
+        # if smallest_index has a lower value than our default i, 
+        if smallest_index != i:
+            # then switch those items
+            arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+
         
 
     return arr
